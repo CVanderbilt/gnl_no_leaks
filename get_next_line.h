1f3f36811212e3e07655727b6bf55da36561eb0c
Eduardo Herrero gonzalez-barros <eherrero@c3r7s2.42madrid.com>
2020-02-26 14:46:24 +0100

    k

--- get_next_line.h
+++ get_next_line.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eherrero <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/16 15:28:25 by eherrero          #+#    #+#             */
+/*   Updated: 2019/12/16 15:28:57 by eherrero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # include <unistd.h>
@@ -11,20 +23,14 @@ typedef struct		s_list
 	char			*str;
 	struct s_list	*next;
 }					t_list;
-
 t_list				*ft_get_list(t_list **head, int fd);
-
 int					get_next_line(int fd, char **line);
 int					get_next_line_rec(int fd, char **line);
-
+int					ft_get_pos(char *str, char *set);
+int					ft_aux(char **line, t_list *tmp, int pos);
+int					ft_supr_lst(t_list **head, t_list *to_del);
 char				*ft_strdup(char *str);
 char				*ft_strndup(char *str, int n);
 char				*ft_strjoin_free(char *str1, char *str2, char c);
 char				*ft_strjoin_free_aux(char *str1, char *str2, char c);
-
-int					ft_get_pos(char *str, char *set);
-int					ft_aux(char **line, t_list *tmp, int pos);
-int					ft_supr_lst(t_list **head, t_list *to_del);
-
-
 #endif
